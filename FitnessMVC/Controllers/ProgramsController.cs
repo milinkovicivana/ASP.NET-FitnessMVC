5f5b44f108b6f8c43a579d7d83a59d323edf3760
ivana <ivanamilinkovic6@gmail.com>
2019-05-02 15:36:17 +0200

    Odradjen index programa

---
+++ FitnessMVC/Controllers/ProgramsController.cs
@@ -0,0 +1,41 @@
+﻿using FitnessMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using FitnessMVC.ViewModels;
+
+namespace FitnessMVC.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProgramsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var programs = _context.Programs.ToList();
+
+            var types = _context.ProgramTypes.ToList();
+
+            var ViewModel = new IndexProgramViewModel
+            {
+                Programs = programs,
+                ProgramTypes = types
+            };
+
+            return View(ViewModel);
+        }
+    }
+}
---
+++ FitnessMVC/ViewModels/IndexProgramViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FitnessMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessMVC.ViewModels
+{
+    public class IndexProgramViewModel
+    {
+        public IEnumerable<ProgramType> ProgramTypes { get; set; }
+        public IEnumerable<Program> Programs { get; set; }
+    }
+}
